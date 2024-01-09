bd375f3b29b17812da8ea33764fa351f95d8c65c
IQBAL CAHYO GUMILAR <iqbal.gumelar@siloamhospitals.com>
2024-01-09 14:02:45 +0700

    Update index.blade.php

--- resources/views/dashboard/files/index.blade.php
+++ resources/views/dashboard/files/index.blade.php
@@ -135,7 +135,7 @@
   copyText.setSelectionRange(0, 99999); // For mobile devices
 
    // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+   window.navigator.clipboard.writeText(copyText.value);
 
   // Alert the copied text
   alert("Copied the text: " + copyText.value);
